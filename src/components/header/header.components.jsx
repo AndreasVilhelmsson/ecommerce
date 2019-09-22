0315e71240182f908af958b77e7e6dd777a63738
Andreas Vilhelmsson <addevilhelmsson@hotmail.com>
2019-09-22 22:26:04 +0200

    added cart dropdown to our application

--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { auth } from "../../firebase/firebase.utils";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
+//import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { connect } from "react-redux";
 import "./header.styles.scss";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -27,12 +30,15 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </nav>
+      {hidden ? null : <CartDropDown />}
     </div>
   );
 };
 // high order functions are functions that take components as arguments and return a new supt up component
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 export default connect(mapStateToProps)(Header);
